e3477115ff93a16954b90d35f986c1dea783f96e
2001brenda <138245937+2001brenda@users.noreply.github.com>
2023-08-04 13:50:55 -0500

    producto.py

---
+++ producto.py
@@ -0,0 +1,27 @@
+def input_int(mensaje, mensaje_error):
+    while True:
+        try:
+            v = int(input(mensaje))
+        except ValueError:
+            print(mensaje_error)
+        else:
+            return v
+
+class Productos:
+    def __init__(self, nombre, marca, precio, cantidad):
+        self.nombre = nombre
+        self.marca = marca
+        self.precio = precio
+        self.cantidad = cantidad
+    
+    def info(self):
+        return f"{self.nombre} {self.marca} {self.cantidad}"
+    
+    def actualizar_cantidad(self, cantidad_vendida):
+        self.cantidad = self.cantidad - cantidad_vendida
+    
+p = Productos("Teclado", "HP", 50, 300)
+l = Productos("Laptop", "HP", 200, 300)
+#print(p.info())
+#p.actualizar_cantidad(5)
+#print(p.info())
